4c6c90c90b6c13611e8bb67ed3956ee3f8af95fc
George Okello <okellogeorge40@yahoo.com>
2018-01-08 03:27:20 -0600

    Add Class Lib for Postgres Data Models

--- BballData/BballContext.cs
+++ BballData/BballContext.cs
@@ -9,6 +9,6 @@ namespace BballData
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Team { get; set; }
+        public string TeamLoc { get; set; }
     }
 }
