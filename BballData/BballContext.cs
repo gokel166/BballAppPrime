f3ab71a0a9dd8c19c887d0d458698643c2bf8aa2
George Okello <okellogeorge40@yahoo.com>
2018-01-08 03:26:29 -0600

    Add class library for Postgres Data models

---
+++ BballData/BballContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Npgsql;
+using Npgsql.EntityFrameworkCore;
+
+namespace BballData
+{
+    public class BballContext
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Team { get; set; }
+    }
+}
