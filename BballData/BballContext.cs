343d9161970c28b755f76c873f73d98f009aa271
George Okello <okellogeorge40@yahoo.com>
2018-01-08 03:43:05 -0600

    Add Context and Player Class

--- BballData/BballContext.cs
+++ BballData/BballContext.cs
@@ -6,16 +6,6 @@ namespace BballData
 {
     public class BballContext
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string TeamName { get; set; }
-        public int  JerseyNo { get; set; }
-        public string Position { get; set; }
-        public double Height { get; set; }
-        public DateTime DOB { get; set; }
-        public int Weight { get; set; }
-        public int Age { get; set; }
-        public string Drafted { get; set; }
+        public int Id { get; set; }        
     }
 }
---
+++ BballData/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BballData.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string TeamName { get; set; }
+        public int JerseyNo { get; set; }
+        public string Position { get; set; }
+        public double Height { get; set; }
+        public DateTime DOB { get; set; }
+        public int Weight { get; set; }
+        public int Age { get; set; }
+        public string Drafted { get; set; }
+    }
+}
